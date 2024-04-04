bb2e2bf45a181c053b59e0091a2694df764a3fb7
SonLoc1506 <sonloc15.06.2k3@gmail.com>
2024-04-04 12:50:30 +0700

    Tao trang lien he moi

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -63,58 +63,60 @@
         </button>
       </div>
     </Form>
-  </template>
+</template>
     
-    <script>
-  import * as yup from "yup";
-  import { Form, Field, ErrorMessage } from "vee-validate";
-  export default {
-    components: {
-      Form,
-      Field,
-      ErrorMessage,
+<script>
+import * as yup from "yup";
+import { Form, Field, ErrorMessage } from "vee-validate";
+export default {
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
+  emits: ["submit:contact", "delete:contact"],
+  props: {
+    mode: { type: String, required: true },
+    contact: { type: Object, required: true },
+  },
+  data() {
+    const contactFormSchema = yup.object().shape({
+      name: yup
+        .string()
+        .required("Tên phải có giá trị.")
+        .min(2, "Tên phải ít nhất 2 ký tự.")
+        .max(50, "Tên có nhiều nhất 50 ký tự."),
+      email: yup
+        .string()
+        .email("E-mail không đúng.")
+        .max(50, "E-mail tối đa 50 ký tự."),
+      address: yup.string().max(100, "Địa chỉ tối đa 100 ký tự."),
+      phone: yup
+        .string()
+        .matches(
+          /((09|03|07|08|05)+([0-9]{8})\b)/g,
+          "Số điện thoại không hợp lệ."
+        ),
+    });
+    return {
+      // Chúng ta sẽ không muốn hiệu chỉnh props, nên tạo biến cục bộ
+      // contactLocal để liên kết với các input trên form
+      contactLocal: this.contact,
+      contactLocal: this.mode === "add" ? {} : {...this.contact},
+      contactFormSchema,
+    };
+  },
+  methods: {
+    submitContact() {
+      this.$emit("submit:contact", this.contactLocal);
     },
-    emits: ["submit:contact", "delete:contact"],
-    props: {
-      contact: { type: Object, required: true },
+    deleteContact() {
+      this.$emit("delete:contact", this.contactLocal.id);
     },
-    data() {
-      const contactFormSchema = yup.object().shape({
-        name: yup
-          .string()
-          .required("Tên phải có giá trị.")
-          .min(2, "Tên phải ít nhất 2 ký tự.")
-          .max(50, "Tên có nhiều nhất 50 ký tự."),
-        email: yup
-          .string()
-          .email("E-mail không đúng.")
-          .max(50, "E-mail tối đa 50 ký tự."),
-        address: yup.string().max(100, "Địa chỉ tối đa 100 ký tự."),
-        phone: yup
-          .string()
-          .matches(
-            /((09|03|07|08|05)+([0-9]{8})\b)/g,
-            "Số điện thoại không hợp lệ."
-          ),
-      });
-      return {
-        // Chúng ta sẽ không muốn hiệu chỉnh props, nên tạo biến cục bộ
-        // contactLocal để liên kết với các input trên form
-        contactLocal: this.contact,
-        contactFormSchema,
-      };
-    },
-    methods: {
-      submitContact() {
-        this.$emit("submit:contact", this.contactLocal);
-      },
-      deleteContact() {
-        this.$emit("delete:contact", this.contactLocal.id);
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
     
-  <style scoped>
-  @import "@/assets/form.css";
-  </style>
+<style scoped>
+@import "@/assets/form.css";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,13 @@ const routes = [
         name: "contact.edit",
         component: () => import("@/views/ContactEdit.vue"),
         props: true // Truyền các biến trong $route.params vào làm props
-        },
+    },
+    {
+        path: "/contacts-create",
+        name: "contact.create",
+        component: () => import("@/views/ContactAdd.vue"),
+        props: true
+    }
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
